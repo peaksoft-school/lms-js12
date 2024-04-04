1ba8c4290a99f83c716e7890f00b664b44f1cd2c
Rakatova-Nuraiym <nurajymrakatova@gmail.com>
2024-04-04 14:53:30 +0600

    updated start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import LayoutAdmin from "./pagesAdmin/components/layout/LayoutAdmin";
 import LayoutInstrutor from "./pagesInstructor/components/layout/LayoutInstrutor";
 import LayoutStudents from "./pagesStudents/components/layout/LayoutStudents";
@@ -5,9 +6,11 @@ import LayoutStudents from "./pagesStudents/components/layout/LayoutStudents";
 function App() {
   return (
     <>
-      <LayoutAdmin />
-      <LayoutInstrutor />
-      <LayoutStudents />
+      <Routes>
+        <Route path="/*" element={<LayoutStudents />} />
+        <Route path="/admin/*" element={<LayoutAdmin />} />
+        <Route path="/instructor/*" element={<LayoutInstrutor />} />
+      </Routes>
     </>
   );
 }
