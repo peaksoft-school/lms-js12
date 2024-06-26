f9592ae4db8491b4a1b765cb8e7dfd420df2dd8a
Rakatova-Nuraiym <nurajymrakatova@gmail.com>
2024-06-26 14:30:28 +0600

    update

--- src/pagesAdmin/components/pages/courseSection/Course.tsx
+++ src/pagesAdmin/components/pages/courseSection/Course.tsx
@@ -128,7 +128,7 @@ const Courses: FC = () => {
 																			<p
 																				style={{
 																					width: '100%',
-																					maxWidth: '100px',
+																					maxWidth: '150px',
 																					textOverflow: 'ellipsis',
 																					overflow: 'hidden'
 																				}}
